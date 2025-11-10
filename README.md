129b4153fb4c9f898a03d6c9e79c843f54126b1a
anders-eriksen <averiksen@proton.me>
2025-11-10 11:06:35 +0100

    Build a bit more structure around the whole thing, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # wind-assignment-2
-Electro assignment - Wind Farm
+## Electro assignment - Wind Farm
+
+### Code structure
+***main.ipynb*** is the main notebook.
+
+***toolbox.py*** hold class **Tools** which is a collection of functions we can call and use throughout the main script.
+
+***equations.ipynb*** has the derivation of the three equations from the grid (right) side of the system.
+
+### Tasks
+- [x] Code how to solve for power on generator side
+  - [ ] Rewrite into callable function, inputs are windspeed and/or omega.
+- [x] Derive three equations on the grid side.
+  - [ ] Find out a way to verify the three functions, maybe speak with Nenad?
+- [ ] Write function in toolbox.py to output all impedances in system **Tools.get_impedances()**
+- [ ] Implement solver
+- [ ] Put the whole dang thing into a for loop to compute over range of wind speeds.
+
---
+++ toolbox.py
@@ -0,0 +1,49 @@
+# Article on how to solve non-linear equations in Python: https://coderivers.org/blog/fsolve-python/
+
+from scipy.optimize import fsolve
+import numpy as np
+
+class Tools:
+    def equations(VS, *P):
+        '''
+        Build equations for the solver of non-linear equations to use.
+        :param vars: Input voltages V_S1 and V_S2
+        :param P1: Real Power input from Turbine 1
+        :param P2: Real Power input from Turbine 2
+        :return: equations for V_B, f(V_S1, V_S2) - VB = 0 form
+        '''
+
+        V_S1 = VS[0]
+        V_S2 = VS[1]
+
+        P1 = P[0]
+        P2 = P[1]
+
+        Z11, Z12, Z21, Z22, Z31, Z32, Z41, Z42, Z5, Z6 = self.get_impedances()
+
+        # equations copied from /Anders/equations.ipynb
+        eq1 = (-P1*Z11*Z31 + V_S1**2*Z31 - Z21*(P1*Z11 + P1*Z31 - V_S1**2))/(V_S1*Z31) - V_B
+        eq2 = (-P2*Z12*Z32 + V_S2**2*Z32 - Z22*(P2*Z12 + P2*Z32 - V_S2**2))/(V_S2*Z32)
+        eq3 = Z41*Z42*(P1*V_S2*Z11*Z22*Z5 + P1*V_S2*Z22*Z31*Z5 + P2*V_S1*Z12*Z31*Z5 + P2*V_S1*Z22*Z31*Z5 + V_POC*V_S1*V_S2*Z22*Z31 - V_S1**2*V_S2*Z22*Z5 - V_S1*V_S2**2*Z31*Z5)/(V_S1*V_S2*Z22*Z31*(Z41*Z42 + Z41*Z5 + Z42*Z5))
+
+        return [eq1, eq2, eq3]
+
+    def get_impedances(self):
+        '''
+        Calculate impedances of the system
+        :return: Z
+        '''
+
+        # Calculate all impedances
+        Z11 = 1
+        Z12 = 1
+        Z21 = 1
+        Z22 = 1
+        Z31 = 1
+        Z32 = 1
+        Z41 = 1
+        Z42 = 1
+        Z5 = 1
+        Z6 = 1
+
+        return Z11, Z12, Z21, Z22, Z31, Z32, Z41, Z42, Z5, Z6
